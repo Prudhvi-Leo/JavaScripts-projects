d8f6b47732ffe370912ff9d9f3a914fa15aeca57
Prudhvi Rao <thallaprudhvi15@gmail.com>
2021-01-17 19:49:31 +0530

    changes

---
+++ backgroundslider/app.js
@@ -0,0 +1,35 @@
+const body=document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+let activeSlide = 0
+setBgToBody()
+function setBgToBody(){
+body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+                            
+                        }
+function setSlides(){
+    slides.forEach((slide)=>{
+        slide.classList.remove('.active')
+    })
+    slides[activeSlide].classList.add('active')
+}
+rightBtn.addEventListener('click',()=>{
+ 
+    activeSlide++;
+    if(activeSlide>2){
+        activeSlide =2;
+    }
+    setBgToBody();
+    setSlides()
+})
+
+leftBtn.addEventListener('click',()=>{
+
+    activeSlide--;
+    if(activeSlide<0){
+        activeSlide=0;
+    }
+    setBgToBody();
+    setSlides()
+})

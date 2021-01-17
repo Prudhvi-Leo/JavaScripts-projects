358fa8d5170050923735fa7bc1acc058bc28c2da
Prudhvi Rao <thallaprudhvi15@gmail.com>
2021-01-18 00:01:29 +0530

    changes

---
+++ drag&drop/app.js
@@ -0,0 +1,32 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+fill.addEventListener('dragstart',dragStart)
+fill.addEventListener('dragend',dragEnd)
+for(const empty of empties){
+    empty.addEventListener('dragover',dragOver)
+    empty.addEventListener('dragenter',dragEnter)
+    empty.addEventListener('dragleave',dragLeave)
+    empty.addEventListener('drop',dragDrop)
+}
+
+function dragStart(){
+        this.className += ' hold'
+        setTimeout(()=> this.className= 'invisible',0)
+}
+function dragDrop(){
+    
+    this.append(fill)
+}
+function dragEnter(e){
+    e.preventDefault()
+    this.className += ' hovered'
+}
+function dragLeave(){
+    this.className = 'empty'
+}
+function dragOver(e){
+    e.preventDefault()
+}
+function dragEnd(){
+    this.className = 'fill'
+}
